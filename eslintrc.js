5e0436d5e7bd0f94ece2c8d69967fdd2b8cf6359
Alan Jackson <ajackson@wework.com>
2016-06-01 22:09:20 -0400

    updated to new eslint format

---
+++ eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true
+    }
+  },
+  'rules': {
+    'indent': [2, 2],
+    'quotes': [2, 'single'],
+    'linebreak-style': [2, 'unix'],
+    'semi': [2, 'always'],
+    'no-console': 0
+  },
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'plugins': [
+    'react'
+  ]
+};
