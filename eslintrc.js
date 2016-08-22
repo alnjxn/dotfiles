00cb08ed229f75a23fbc32efcca5d966a9020d8f
Alan Jackson <ajackson@wework.com>
2016-08-22 14:55:28 -0400

    bare bones minimal eslintrc

--- eslintrc.js
+++ eslintrc.js
@@ -1,29 +1,16 @@
 module.exports = {
-  'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'jsx': true
-    }
+  parserOptions: {
   },
-  'rules': {
-    'indent': [2, 2],
-    'quotes': [2, 'single'],
-    'linebreak-style': [2, 'unix'],
-    'semi': [2, 'always'],
-    'no-console': 0
+  rules: {
   },
-  'env': {
-    'es6': true,
-    'node': true,
-    'mocha': true,
-    'browser': true
+  env: {
+    mocha: true,
+    browser: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
+  extends: [
+    'airbnb',
   ],
-  'plugins': [
-    'react'
-  ]
+  plugins: [
+  ],
 };
+
