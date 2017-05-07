5a55dada8a747be5ed9e3057339e29c446a4dc25
Alan Jackson <ajackson@wework.com>
2017-05-07 18:00:29 -0400

    simplified eslint

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,22 +1,8 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  rules: {
-  },
   env: {
     mocha: true,
     browser: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:react/recommended',
-  ],
-  plugins: [
-    'react',
-  ],
+  extends: 'airbnb',
 };
 
